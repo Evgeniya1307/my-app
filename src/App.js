a57192e0680f258be206ca02c0a2445b1504de4b
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-27 12:34:46 +0400

    onChange={event=>event.target}//onChange первым параметром принимют эвент  у которого есть поле таргет  сам домэлемент вэлью значение которое в нём находится

--- src/App.js
+++ src/App.js
@@ -2,60 +2,41 @@ import { useState } from "react";
 
 // добавляю ещё одно состояние
 
-function App (){
-  const [likes, setLikes]= useState(0)
-const [value, setValue]=useState('') // будем его менять в инпуте
-  
-  
-  function increment(){
-    setLikes(likes+1)
+function App() {
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState("текст в инпуте"); // будем его менять в инпуте
+
+  function increment() {
+    setLikes(likes + 1);
   }
 
-  function decrement(){
-    setLikes(likes-1)
+  function decrement() {
+    setLikes(likes - 1);
   }
-return (
-  <div className="App">
-  <h1>{likes}</h1>
-  <button onClick={increment}>increment</button>
-  <button onClick={decrement}>decrement</button>
-  </div>
-)
-
+  return (
+    <div className="App">
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input type="text" value={value}
+      onChange={event=>event.target}//onChange первым параметром принимют эвент  у которого есть поле таргет  сам домэлемент вэлью значение которое в нём находится
+      /> 
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  );
 }
 
 
+export default App;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// счётчик с 2мя кнопками 
+// счётчик с 2мя кнопками
 // function App() {
 // const [likes, setLikes]= useState(0) // деструктуризация
 
 // function increment(){
 // setLikes(likes+1) //напрямую не меняем передаём в функцию
 // }
-  
+
 // function decrement(){
 // setLikes(likes-1)
 // }
@@ -65,7 +46,7 @@ return (
 //     <h1>{likes}</h1>
 //     <button onClick={increment}>increment</button>
 //     <button onClick={decrement}>decrement</button>
-     
+
 //     </div>
 //   );
 // }
