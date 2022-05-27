c8e3779581f5d0e331aa2b0f37f5674415db7b46
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-27 12:43:21 +0400

    const [state, setstate] = useState(0) // создала состояние

---
+++ public/components/Counter.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const Counter = function() {
+const [state, setstate] = useState(0) // создала состояние 
+    
+    return (
+
+    <div>
+    </div>
+    )   
+   
+}
+
+
+
+export default Counter;
+
