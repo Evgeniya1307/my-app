9c13fc8387ea05ed3068a87bd5377d4cd6208cc9
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-27 13:19:33 +0400

    #root { display: flex; justify-content: center;  }

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import { useState } from "react";
+import Counter from './components/Counter'
+import './Styles/App.css';
 
-// добавляю ещё одно состояние
 
 function App() {
-  const [likes, setLikes] = useState(0);
   const [value, setValue] = useState("текст в инпуте"); // будем его менять в инпуте
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input type="text" value={value}
-      onChange={event=>event.target}//onChange первым параметром принимют эвент  у которого есть поле таргет  сам домэлемент вэлью значение которое в нём находится
-      /> 
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
+    <div className="post">
+    <div className="post_content">
+    <strong>1. JavaScript</strong>
+    <div>
+    JavaScript - язык программирования
+    </div>
+    </div>
+    < div className="post_btns">
+    <button>Удалить</button>
+    </div>
+    </div>
+
     </div>
   );
 }
 
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
 
 // счётчик с 2мя кнопками
 // function App() {
@@ -52,3 +61,17 @@ export default App;
 // }
 
 // export default App;
+
+
+
+// добавляю ещё одно состояние
+//const [value, setValue] = useState("текст в инпуте"); // будем его менять в инпуте
+// return (
+//   <div className="App">
+//     <h1>{likes}</h1>
+//     <h1>{value}</h1>
+//     <input type="text" value={value}
+//     onChange={event=> setValue(event.target.value)}//onChange первым параметром принимют эвент  у которого есть поле таргет  сам домэлемент вэлью значение которое в нём находится теперь что пишу в инпут отображается
+//     /> 
+//   </div>
+// );
